692afcbf2809f598ae0ce89798bd94e702c9c3a3
781246808@qq.com <781246808@qq.com>
2018-12-22 15:33:47 +0800

    provider注释添加

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import rootReducer from './reducers'
 // store连接action和reducer
 const store = createStore(rootReducer)
 
+// Provider 下层级组件才能使用 react-redux 的api,如connect
 ReactDom.render(
   <Provider store={store}>
   <App />
